ffd938cc9c4bd34653a92c44e6918321f52b2909
Vaibhaw Krishna <vaibhaw.krishna.1@gmail.com>
2024-05-22 00:02:07 +0530

    Initial commit

---
+++ backend/db.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const connectionString = "mongodb://localhost:27017";
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 6
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 6
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxlength: 30
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  },
+});
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {
+    User
+};
---
+++ backend/index.js
@@ -0,0 +1,4 @@
+// e1db9182f28a06c3b224fe752458f401eb4d5f1749922457dfe640a97ae3d07a
+const express = require("express");
+
+const app = express()
